27960892606931f4eac1c5f5e2f459332b1a7e4a
Harshit Singh <harshitks2203@gmail.com>
2020-02-24 23:43:12 -0500

    updated song change logic

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
       <section class="player">
         <h2 class="song-title"> {{ current.title }} - <span> {{ current.artist }} </span> </h2>
         <div class="contro">
-          <button class="prev">Prev</button>
+          <button class="prev" @click="prev">Prev</button>
           <button class="play" v-if="!isPlaying" @click="play">Play</button>
           <button class="pause" v-else @click="pause">Pause</button>
-          <button class="next">Next</button>
+          <button class="next" @click="next">Next</button>
         </div>
       </section>
       <section class="playlist">
@@ -52,6 +52,22 @@ export default {
     pause() {
       this.player.pause();
       this.isPlaying = false;
+    },
+    next() {
+      this.index++;
+      if (this.index > this.songs.length -1) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
     }
   },
   created() {
